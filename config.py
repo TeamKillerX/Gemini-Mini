b9419ac423ad8e38a18f2b9bb032115ab81ecf45
Randy Dev <killerx@randydev.my.id>
2025-05-08 22:53:36 +0700

    v1 mini

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+MONGO_URL = os.getenv("MONGO_URL")
 
-if not all([API_ID, API_HASH, BOT_TOKEN, GOOGLE_API_KEY]):
+if not all([API_ID, API_HASH, BOT_TOKEN, GOOGLE_API_KEY, MONGO_URL]):
     raise ValueError("404 Error variables all.")
     exit(1)
