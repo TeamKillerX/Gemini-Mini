305b1f566be3b0c262372fcf75ed2b28f380ffd8
Randy Dev <killerx@randydev.my.id>
2025-05-08 22:22:17 +0700

    v1 mini

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+if not all([API_ID, API_HASH, BOT_TOKEN, GOOGLE_API_KEY]):
+    raise ValueError("404 Error variables all.")
+    exit(1)
